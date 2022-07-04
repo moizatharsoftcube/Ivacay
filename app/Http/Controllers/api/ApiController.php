ce8f70c60881b272a1e6e4adad9f5bfb95fd97d1
moizatharsoftcube <moizatharecoders12@gmail.com>
2022-07-05 00:50:07 +0500

    Revert "validator response json"
    This reverts commit 5bfdcca90f9b605f71af142931e6b21819a2d2b0.

--- app/Http/Controllers/api/ApiController.php
+++ app/Http/Controllers/api/ApiController.php
@@ -107,7 +107,7 @@ class ApiController extends EmailController
 
         if ($reviews && $review_avg) {
             return response()->json([
-                'data' => $reviews,
+                'reviews' => $reviews,
                 'review_avg' => $review_avg,
             ], 200);
         } else {
@@ -126,7 +126,7 @@ class ApiController extends EmailController
         );
         $validator = Validator::make($request->all(), $rules);
         if ($validator->fails()) {
-            return response()->json($validator->errors(),422);
+            return $validator->errors();
         }
         $review_model = new ReviewModel();
         $review_model->user_id = Auth::user()->id;
